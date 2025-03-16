80b01273cc31627b442b5808dac74b0dacbbf007
Blond <87145006+182blond@users.noreply.github.com>
2025-03-16 20:39:25 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,4 @@
 // ---- config/db.js ----
 module.exports = {
-    url: 'mongodb://localhost:27017/vue_project_db'
-    // Para producción, usar una variable de entorno:
-    // url: process.env.MONGODB_URI
+    url: process.env.MONGODB_URI
 };
